c54ecbee855bc8ed42e834997864b9804b06e22f
Bryan McKelvey <bryan.mckelvey@gmail.com>
2018-04-29 12:54:31 +0900

    Remove non-word character replace

--- charactertrigramfuzzyset/__init__.py
+++ charactertrigramfuzzyset/__init__.py
@@ -3,29 +3,31 @@
 #   https://github.com/axiak/fuzzyset
 #
 # That library hasn't been updated much and has some inefficiencies that I
-# wanted to address in a pure Python implementation
+# wanted to address in a pure Python implementation.
 
-import re
 from math import sqrt
 from operator import itemgetter
 from collections import defaultdict
 
-_non_word_sub = re.compile(r'[^\w, ]+').sub
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 class CharacterTrigramFuzzySet(object):
     def __init__(self, iterable, max_count=100, relative_threshold=0.5):
         self._match_dict = defaultdict(list)
         self._items = []
+        self._seen = set()
         for value in iterable:
             self._add(value)
-        self.max_count = max_count
-        self.relative_threshold = relative_threshold
+        self._max_count = max_count
+        self._relative_threshold = relative_threshold
 
     def _add(self, value):
         lvalue = value.lower()
-        simplified = '-' + _non_word_sub('', lvalue) + '-'
+        simplified = '-' + lvalue + '-'
+        if lvalue in self._seen:
+            return
+        self._seen.add(lvalue)
         gram_count = len(simplified) - 2
         idx = len(self._items)
         for i in range(gram_count):
@@ -33,7 +35,7 @@ class CharacterTrigramFuzzySet(object):
         self._items.append((sqrt(gram_count), lvalue))
 
     def get(self, value):
-        simplified = '-' + _non_word_sub('', value.lower()) + '-'
+        simplified = '-' + value.lower() + '-'
         gram_count = len(simplified) - 2
         norm = sqrt(gram_count)
 
@@ -48,7 +50,7 @@ class CharacterTrigramFuzzySet(object):
                    for idx, match_score in matches.items()]
         results.sort(reverse=True, key=itemgetter(0))
 
-        threshold = results[0][0] * self.relative_threshold
+        threshold = results[0][0] * self._relative_threshold
         return [(score, lval)
-                for score, lval in results[:self.max_count]
+                for score, lval in results[:self._max_count]
                 if score > threshold]
